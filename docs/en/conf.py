1c54a6500a37716fb68b640cc0461ecba4f8687d
Jean-François Rivière <jean-francois.riviere@openedition.org>
2019-11-29 16:34:20 +0100

    fix conf.py master_doc

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -38,7 +38,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-mster_doc = 'index'
+master_doc = 'index'
 
 language = "en"
 locale_dirs = ["../../locales/"]
